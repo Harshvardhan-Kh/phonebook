629d40c07652e94354437ad140086112dbfcd003
harshvardhan khairnar <harshvardhan@kali.kali.org>
2023-10-17 11:48:00 +0530

    useEffect and filteration added

--- src/views/App.js
+++ src/views/App.js
@@ -1,19 +1,24 @@
-
 import ContactCard from "../components/ContactCard";
 import { CallBook } from "../Data/CallBook";
 import React, { useEffect, useState } from "react";
 
-
 const App = () => {
   const [contacts, setContacts] = useState(CallBook);
   const [searchTerm, setSearchTerm] = useState("");
-
+  useEffect(() => {
+    const filteredContact = CallBook.filter((contact) => {
+      const name = contact.name.toLocaleLowerCase();
+      const mobile = contact.mobile.toString();
+      const query = searchTerm.toLocaleLowerCase();
+      return name.includes(query) || mobile.includes(query);
+    });
+    setContacts(filteredContact);
+  }, [searchTerm]);
 
   return (
     <>
       <div className="container">
-
-      <div className="heading">
+        <div className="heading">
           <h2 className="title">Harshvardhan's</h2>
           <h3 className="phoneBook">Phone Book</h3>
         </div>
@@ -26,10 +31,8 @@ const App = () => {
             onChange={(e) => {
               setSearchTerm(e.target.value);
             }}
-            
           />
         </div>
-
 
         <div className="c">
           {contacts.map((contacts, index) => {
