30ead81124b395c00420a2212ae48fffd08d7415
harshvardhan khairnar <harshvardhan@kali.kali.org>
2023-10-17 11:43:00 +0530

    contact card created and css implemented

--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,52 @@
-import React from 'react'
+
 import ContactCard from "../components/ContactCard";
 import { CallBook } from "../Data/CallBook";
+import React, { useEffect, useState } from "react";
+
 
 const App = () => {
+  const [contacts, setContacts] = useState(CallBook);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
   return (
-    <div><ContactCard/></div>
-  )
-}
+    <>
+      <div className="container">
+
+      <div className="heading">
+          <h2 className="title">Harshvardhan's</h2>
+          <h3 className="phoneBook">Phone Book</h3>
+        </div>
+        <div className="inputContainer">
+          <input
+            type="text"
+            className="inputSearch"
+            placeholder="search here"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+            
+          />
+        </div>
+
+
+        <div className="c">
+          {contacts.map((contacts, index) => {
+            const { randomClass, name, mobile } = contacts;
+            return (
+              <ContactCard
+                key={index}
+                name={name}
+                mobile={mobile}
+                randomClass={randomClass}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
